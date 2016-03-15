96f409ad3bd7bbc768c0f94502a87a09bc4e4e38
feng <f2010525@gmail.com>
2016-03-16 01:30:31 +0800

    初始提交

---
+++ portal/index(backup).php
@@ -0,0 +1,163 @@
+<?PHP exit('Access Denied');?>
+<!--{subtemplate common/header}-->
+<style id="diy_style" type="text/css"></style>
+<!--[diy=wxd01]--><div id="wxd01" class="area"></div><!--[/diy]-->
+<div class="clearfix wxd_warp960">
+<div class="wxd_warp370">
+	<div style="margin-top:10px;"><!--[diy=wxd02]--><div id="wxd02" class="area"></div><!--[/diy]--></div>
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>城市生活向导</h2></div>
+		<div class="wxd_height279">
+			<div class="wxd_ico"><!--[diy=wxd03]--><div id="wxd03" class="area"></div><!--[/diy]--></div>
+			<div class="wxd_warp214">
+			<!--[diy=wxd04]--><div id="wxd04" class="area"></div><!--[/diy]-->
+			</div>
+		</div>
+	</div>
+</div>
+<div class="wxd_hotnew">
+	<div class="wxd_hottitle"><h2>今日热点</h2><script type="text/javascript">var d = new Date();document.write(d.getFullYear()+'年'+(d.getMonth()+1)+'月'+d.getDate()+'日'+' 星期'+'日一二三四五六'.charAt(new Date().getDay()));</script></div>
+	<!--[diy=wxd05]--><div id="wxd05" class="area"></div><!--[/diy]-->
+</div>
+<div class="wxd_warp200">
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>社区精选</h2><span><a href="#" target=_blank>更多&gt;&gt;</a></span></div>
+		<div class="wxd_height260">
+		<!--[diy=wxd06]--><div id="wxd06" class="area"></div><!--[/diy]-->
+		</div>
+	</div>
+	<div class="wxd_ad">
+	<!--[diy=wxd07]--><div id="wxd07" class="area"></div><!--[/diy]-->
+	</div>
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>社区动态</h2><span><a href="#" target=_blank>更多&gt;&gt;</a></span></div>
+		<div class="wxd_height260 wxd_height119">
+		<!--[diy=wxd08]--><div id="wxd08" class="area"></div><!--[/diy]-->
+		</div>
+	</div>
+</div>
+</div>
+<!--[diy=wxd09]--><div id="wxd09" class="area"></div><!--[/diy]-->
+<div class="clearfix wxd_warp960">
+<div class="wxd_borderbox">
+	<div class="wxd_title"><h2>社区贴图</h2><span>以敏锐的感观独特的视角 用光影色捕捉生活中的点点滴滴</span></div>
+	<div class="clearfix wxd_imgslist">
+		<!--[diy=wxd10]--><div id="wxd10" class="area"></div><!--[/diy]-->
+	</div>
+</div>
+</div>
+<div class="clearfix wxd_warp960">
+<div class="wxd_warp760">
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>休闲娱乐</h2><span><a href="#" target=_blank>进入频道&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+				<div class="wxd_warp122">
+				<!--[diy=wxd11]--><div id="wxd11" class="area"></div><!--[/diy]-->
+				</div>
+				<div class="wxd_warp214 wxd_big">
+				<!--[diy=wxd12]--><div id="wxd12" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>城市美食</h2><span><a href="#" target=_blank>进入频道&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+				<div class="wxd_warp122">
+					<!--[diy=wxd13]--><div id="wxd13" class="area"></div><!--[/diy]-->
+				</div>
+				<div class="wxd_warp214 wxd_big">
+					<!--[diy=wxd14]--><div id="wxd14" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>情感婚姻</h2><span><a href="#" target=_blank>进入频道&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+				<div class="wxd_warp122">
+					<!--[diy=wxd15]--><div id="wxd15" class="area"></div><!--[/diy]-->
+				</div>
+				<div class="wxd_warp214 wxd_big">
+					<!--[diy=wxd16]--><div id="wxd16" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>妈咪宝贝</h2><span><a href="#" target=_blank>进入频道&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+				<div class="wxd_warp122">
+					<!--[diy=wxd17]--><div id="wxd17" class="area"></div><!--[/diy]-->
+				</div>
+				<div class="wxd_warp214 wxd_big">
+					<!--[diy=wxd18]--><div id="wxd18" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>房产楼市</h2><span><a href="#" target=_blank>进入频道&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+				<div class="wxd_warp122">
+					<!--[diy=wxd19]--><div id="wxd19" class="area"></div><!--[/diy]-->
+				</div>
+				<div class="wxd_warp214 wxd_big">
+					<!--[diy=wxd20]--><div id="wxd20" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>车迷乐园</h2><span><a href="#" target=_blank>进入频道&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+				<div class="wxd_warp122">
+					<!--[diy=wxd21]--><div id="wxd21" class="area"></div><!--[/diy]-->
+				</div>
+				<div class="wxd_warp214 wxd_big">
+					<!--[diy=wxd22]--><div id="wxd22" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="wxd_warp200">
+	<div class="wxd_ad">
+	<!--[diy=wxd23]--><div id="wxd23" class="area"></div><!--[/diy]-->
+	</div>
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>打折优惠</h2><span><a href="#" target=_blank>更多&gt;&gt;</a></span></div>
+		<div class="wxd_height260 wxd_height229">
+		<!--[diy=wxd24]--><div id="wxd24" class="area"></div><!--[/diy]-->
+		</div>
+	</div>
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>游山玩水</h2><span><a href="#" target=_blank>更多&gt;&gt;</a></span></div>
+		<div class="wxd_height260 wxd_height215">
+		<!--[diy=wxd25]--><div id="wxd25" class="area"></div><!--[/diy]-->
+		</div>
+	</div>
+	<div class="wxd_ad">
+	<!--[diy=wxd27]--><div id="wxd27" class="area"></div><!--[/diy]-->
+	</div>
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>网友中心</h2><span><a href="#" target=_blank>更多&gt;&gt;</a></span></div>
+		<div class="wxd_height260 wxd_height165">
+		<!--[diy=wxd26]--><div id="wxd26" class="area"></div><!--[/diy]-->
+		</div>
+	</div>
+</div>
+</div>
+<!--[diy=wxd28]--><div id="wxd28" class="area"></div><!--[/diy]-->
+<div class="clearfix wxd_warp960">
+<div class="wxd_borderbox" style="margin-bottom:10px;">
+<div class="wxd_title"><code>友情链接</code><span>友情链接本站要求: 排名50万内,百度收录1000以上  联系QQ:88888888  邮箱:admin@admin.com</span></div>
+<!--[diy=wxd34]--><div id="wxd34" class="area"></div><!--[/diy]--></div>
+</div>
+<!--{subtemplate common/footer}-->
---
+++ portal/index.php
@@ -0,0 +1,151 @@
+<?php echo '薇晓朵网络工作室商业模板保护！请购买正版模板，http://www.Weixiaoduo.com/';exit;?>
+<!--{subtemplate common/header}-->
+<style id="diy_style" type="text/css"></style>
+<!--[diy=wxd01]--><div id="wxd01" class="area"></div><!--[/diy]-->
+<div class="clearfix wxd_warp960">
+<div class="wxd_warp370">
+	<div style="margin-top:10px;"><!--[diy=wxd02]--><div id="wxd02" class="area"></div><!--[/diy]--></div>
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>本地生活推荐</h2></div>
+		<div class="wxd_height279">
+			<div class="wxd_warp348">
+			<!--[diy=wxd04]--><div id="wxd04" class="area"></div><!--[/diy]-->
+			</div>
+		</div>
+	</div>
+</div>
+<div class="wxd_hotnew">
+	<div class="wxd_hottitle"><h2>今日热点</h2><script type="text/javascript">var d = new Date();document.write(d.getFullYear()+'年'+(d.getMonth()+1)+'月'+d.getDate()+'日'+' 星期'+'日一二三四五六'.charAt(new Date().getDay()));</script></div>
+	<!--[diy=wxd05]--><div id="wxd05" class="area"></div><!--[/diy]-->
+</div>
+<div class="wxd_warp200">
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>社区导航</h2><span><a href="#" target=_blank>更多&gt;&gt;</a></span></div>
+		<div class="wxd_height260">
+		<!--[diy=wxd06]--><div id="wxd06" class="area"></div><!--[/diy]-->
+		</div>
+	</div>
+	<div class="wxd_ad">
+	<!--[diy=wxd07]--><div id="wxd07" class="area"></div><!--[/diy]-->
+	</div>
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>社区动态</h2><span><a href="#" target=_blank>更多&gt;&gt;</a></span></div>
+		<div class="wxd_height260 wxd_height101">
+		<!--[diy=wxd08]--><div id="wxd08" class="area"></div><!--[/diy]-->
+		</div>
+	</div>
+</div>
+</div>
+<!--[diy=wxd09]--><div id="wxd09" class="area"></div><!--[/diy]-->
+<div class="clearfix wxd_warp960">
+<div class="wxd_borderbox">
+	<div class="wxd_title"><h2>社区图库</h2><span>当你愿意记录，每个人都是生活的导演。</span></div>
+	<div class="clearfix wxd_imgslist">
+		<!--[diy=wxd10]--><div id="wxd10" class="area"></div><!--[/diy]-->
+	</div>
+</div>
+</div>
+<!--[diy=wxd03]--><div id="wxd03" class="area"></div><!--[/diy]-->
+<div class="clearfix wxd_warp960">
+<div class="wxd_warp760">
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>休闲娱乐</h2><span><a href="#" target=_blank>马上进入&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+
+				<div class="wxd_warp348 wxd_big">
+				<!--[diy=wxd12]--><div id="wxd12" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>特色美食</h2><span><a href="#" target=_blank>马上进入&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+
+				<div class="wxd_warp348 wxd_big">
+					<!--[diy=wxd14]--><div id="wxd14" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>交友征婚</h2><span><a href="#" target=_blank>马上进入&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+
+				<div class="wxd_warp348 wxd_big">
+					<!--[diy=wxd16]--><div id="wxd16" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>亲子乐园</h2><span><a href="#" target=_blank>马上进入&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+
+				<div class="wxd_warp348 wxd_big">
+					<!--[diy=wxd18]--><div id="wxd18" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>房屋租售</h2><span><a href="#" target=_blank>马上进入&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+
+				<div class="wxd_warp348 wxd_big">
+					<!--[diy=wxd20]--><div id="wxd20" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="wxd_warp370">
+		<div class="wxd_borderbox">
+			<div class="wxd_title"><h2>求职招聘</h2><span><a href="#" target=_blank>马上进入&gt;&gt;</a></span></div>
+			<div class="wxd_height279">
+
+				<div class="wxd_warp348 wxd_big">
+					<!--[diy=wxd22]--><div id="wxd22" class="area"></div><!--[/diy]-->
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="wxd_warp200">
+	<div class="wxd_ad">
+	<!--[diy=wxd23]--><div id="wxd23" class="area"></div><!--[/diy]-->
+	</div>
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>特价优惠</h2><span><a href="#" target=_blank>更多&gt;&gt;</a></span></div>
+		<div class="wxd_height260 wxd_height229">
+		<!--[diy=wxd24]--><div id="wxd24" class="area"></div><!--[/diy]-->
+		</div>
+	</div>
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>游山玩水</h2><span><a href="#" target=_blank>更多&gt;&gt;</a></span></div>
+		<div class="wxd_height260 wxd_height215">
+		<!--[diy=wxd25]--><div id="wxd25" class="area"></div><!--[/diy]-->
+		</div>
+	</div>
+	<div class="wxd_ad">
+	<!--[diy=wxd27]--><div id="wxd27" class="area"></div><!--[/diy]-->
+	</div>
+	<div class="wxd_borderbox">
+		<div class="wxd_title"><h2>网友中心</h2><span><a href="#" target=_blank>更多&gt;&gt;</a></span></div>
+		<div class="wxd_height260 wxd_height165">
+		<!--[diy=wxd26]--><div id="wxd26" class="area"></div><!--[/diy]-->
+		</div>
+	</div>
+</div>
+</div>
+<!--[diy=wxd28]--><div id="wxd28" class="area"></div><!--[/diy]-->
+<div class="clearfix wxd_warp960">
+<div class="wxd_borderbox" style="margin-bottom:10px;">
+<div class="wxd_title"><code>友情链接</code><span>友情链接交换，请联系 QQ 123456789 以便及时添加。要求: 排名50万内,百度收录1000以上</span></div>
+<!--[diy=wxd34]--><div id="wxd34" class="area"></div><!--[/diy]--></div>
+</div>
+<!--{subtemplate common/footer}-->
